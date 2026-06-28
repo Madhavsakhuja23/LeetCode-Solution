201e6ad1a2cb9dd227d46b9ceaa2c8da38916000
Madhav Sakhuja <msakhuja22@gmail.com>
2026-06-28 10:10:07 +0530

    Time: 1 ms (61.47%), Space: 47.2 MB (57.23%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.java
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.java
@@ -1,32 +1,42 @@
 class Solution {
     public boolean isBipartite(int[][] graph) {
-        int v = graph.length;
-        int colour[] = new int[v];
-        for(int i=0;i<v;i++){
-            colour[i]=-1;
-        }
-        for(int i=0;i<v;i++){
+        int n = graph.length;
+        int m = graph[0].length;
+        int visited[] = new int[n];
+        // ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        // for(int i=0;i<graph.length;i++){
+        //     for(int j=0;j<graph[i].length;j++){
+        //         adj.get(i).add(j);
+        //         adj.get(j).add(i);
+        //     }
+        // }
+        int colour[] = new int[graph.length];
+        Arrays.fill(colour, -1);
+        for(int i=0;i<graph.length;i++){
             if(colour[i]==-1){
-                if(!dfs(i,0, graph,colour)){
+                if(bfs(i,graph,colour)==false){
                     return false;
                 }
             }
         }
         return true;
     }
-    public boolean dfs(int i, int c, int[][] graph, int colour[]){
-        colour[i]=c;
-        for(int it: graph[i]){
-            if(colour[it]==-1){
-                c=(colour[i]==0?1:0);
-                if(dfs(it,c,graph,colour)==false){
+    public boolean bfs(int node , int[][] graph, int colour[]){
+        Queue<Integer> q = new LinkedList<>();
+        q.add(node);
+        colour[node]=0;
+        while(!q.isEmpty()){
+            int n= q.poll();
+            for(int it: graph[n]){
+                if(colour[it]==-1){
+                    colour[it]=(colour[n]==0?1:0);
+                    q.add(it);
+                }else if(colour[it]==colour[n]){
                     return false;
                 }
             }
-            else if(colour[it]==colour[i]){
-                return false;
-            }
         }
         return true;
+
     }
 }
