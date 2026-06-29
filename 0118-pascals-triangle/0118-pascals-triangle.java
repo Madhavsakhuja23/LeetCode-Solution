9ce3098dde93a33353293732e72b6e6e3c60741e
Madhav Sakhuja <msakhuja22@gmail.com>
2026-06-29 17:28:17 +0530

    Time: 1 ms (93.8%), Space: 43.5 MB (33.09%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.java
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -6,9 +6,9 @@ class Solution {
             res.add(lis);
             for(int j=0;j<=i;j++){
                 if(j==0 || j==i){
-                    res.get(i).add(1);
+                    res.get(i).add(1); // for every last and the first column it will always 1 
                 }else{
-                    lis.add(res.get(i-1).get(j-1)+res.get(i-1).get(j));
+                    lis.add(res.get(i-1).get(j-1)+res.get(i-1).get(j)); // get the previous row get the previous colmun of that and then add with the previous row with same column number 
                 }
             }
         }
