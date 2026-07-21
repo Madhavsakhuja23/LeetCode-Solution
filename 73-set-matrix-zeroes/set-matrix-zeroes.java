cff7698e2405aa761ce93ed530fe8873e9cb1502
Madhav Sakhuja <msakhuja22@gmail.com>
2026-07-21 17:18:40 +0530

    Time: 1 ms (98.35%) | Memory: 47.7 MB (43.69%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,33 +1,22 @@
-class Pair{
-    int i;
-    int j;
-    Pair(int x, int y){
-        i=x;
-        j=y;
-    }
-}
 class Solution {
     public void setZeroes(int[][] matrix) {
         int m = matrix.length;
         int n=matrix[0].length;
-        Stack<Pair> st = new Stack<>();
+        boolean row[] = new boolean[m];
+        boolean col[] = new boolean[n];
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(matrix[i][j]==0){
-                    st.add(new Pair(i, j));
+                    row[i]=true;
+                    col[j]=true;
                 }
             }
         }
-        while(!st.isEmpty()){
-            Pair curr = st.pop();
-            int x=curr.i;
-            int y=curr.j;
-
-            for(int i=0;i<m;i++){
-                matrix[i][y]=0;
-            }
-            for(int i=0;i<n;i++){
-                matrix[x][i]=0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(row[i] || col[j]){
+                    matrix[i][j]=0;
+                }
             }
         }
     }
